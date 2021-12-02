6affe5512ba8ab776ce820058b0fa734aa7196e0
Nemanja Šupljeglav <nsupljeglav@ad.ito.ba>
2021-12-02 09:37:59 +0100

    add map marker

--- src/components/googleMaps/GoogleMapsContainer.js
+++ src/components/googleMaps/GoogleMapsContainer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./googleMaps.css";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { useState } from "react";
+import MarkerMaps from "./MarkerMaps";
 
 const containerStyle = {
   width: "100%",
@@ -11,20 +13,24 @@ const containerStyle = {
   top: "-70px",
   borderRadius: "5px",
 };
+//marker
 
-const center = {
-  lat: 43.343777,
-  lng: 17.807758,
-  zoom: 14,
-};
-
+//marker end
 function TranslationContainer() {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyAD9BnG-MvxatROXPl1JFhF9O0ztnv_Hbg",
   });
-
+  const [zoomMap, setZoomMap] = useState(1);
+  setInterval(() => {
+    setZoomMap(2);
+  }, 10);
   const [map, setMap] = React.useState(null);
+  const center = {
+    lat: 43.343777,
+    lng: 17.807758,
+    zoom: 14 + zoomMap,
+  };
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
@@ -48,6 +54,11 @@ function TranslationContainer() {
         >
           {/* Child components, such as markers, info windows, etc. */}
           <></>
+          {true && <Marker position={{ lat: 43.343777, lng: 17.807758 }} />}
+          {true && <Marker position={{ lat: 43.398777, lng: 17.807758 }} />}
+          {true && <Marker position={{ lat: 43.318777, lng: 17.802758 }} />}
+          {true && <Marker position={{ lat: 43.418777, lng: 17.902758 }} />}
+          {true && <Marker position={{ lat: 43.618777, lng: 17.302758 }} />}
         </GoogleMap>
       ) : (
         <></>
