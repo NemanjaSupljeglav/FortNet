49182f16faf454180b2b58870681ec1bf8e61f58
Nemanja Šupljeglav <nsupljeglav@ad.ito.ba>
2021-12-02 11:06:47 +0100

    start with banner

--- src/pages/Home/HomeContainer.js
+++ src/pages/Home/HomeContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavLinks } from "../../components/NavLinks";
-
+import Bannner from "assets/images/banner1.png";
 export function HomeContainer() {
   return (
     <div>
       <NavLinks />
-      <div>Home page</div>
+      <div className="wrapper-home-page">
+        <img src={Bannner}></img>
+      </div>
     </div>
   );
 }
