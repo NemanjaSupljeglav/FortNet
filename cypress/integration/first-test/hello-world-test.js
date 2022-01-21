3d92357a1566194eaba291533d0806aa596dfe97
N3manjaS <71728105+NemanjaSupljeglav@users.noreply.github.com>
2022-01-21 09:06:04 +0100

    add cypress

---
+++ cypress/integration/first-test/hello-world-test.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("primjer 1", () => {
+  it("nemanja prva proba", () => {});
+
+  it("nemanja prva proba", () => {});
+  it("nemanja prva proba", () => {});
+  it("nemanja prva proba", () => {});
+  it("nemanja prva proba", () => {});
+  it("nemanja prva proba", () => {});
+});
