f11b1142e04d93b1862521e511774e3ac8ba2cc4
N3manjaS <71728105+NemanjaSupljeglav@users.noreply.github.com>
2022-01-25 09:01:07 +0100

    Add table

--- cypress/integration/first-test/hello-world-test.js
+++ cypress/integration/first-test/hello-world-test.js
@@ -1,9 +1,18 @@
 /// <reference types="cypress" />
 
 describe("primjer 1", () => {
-  it("nemanja prva proba", () => {});
+  it("Home and translation, and page load", () => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("Hrvatski").should("exist");
+    cy.contains("Hrvatski").click();
+    cy.contains("NASLOVNA");
+  });
 
-  it("nemanja prva proba", () => {});
+  it("MAPA testing", () => {
+    cy.contains("MAPA").click();
+    cy.contains("English").click();
+    cy.contains("EnoGastro").click();
+  });
   it("nemanja prva proba", () => {});
   it("nemanja prva proba", () => {});
   it("nemanja prva proba", () => {});
--- src/pages/Attractions/AttractionsContainer.js
+++ src/pages/Attractions/AttractionsContainer.js
@@ -1,11 +1,37 @@
 import React from "react";
 import { NavLinks } from "../../components/NavLinks";
-
+import MsterialTable from "material-table";
 export function AttractinosContainer() {
+  const data = [
+    { name: "Ramo", age: 30 },
+    { name: "Mihael", age: 27 },
+    { name: "Sinisa", age: 35 },
+    { name: "Vedran", age: 32 },
+    { name: "Šuke", age: 27 },
+    { name: "Mile", age: 25 },
+    { name: "Andreja", age: 25 },
+    { name: "Dane", age: 36 },
+  ];
+  const columns = [
+    {
+      title: "Name",
+      field: "name",
+    },
+    {
+      title: "Age",
+      field: "age",
+    },
+  ];
   return (
     <div>
       <NavLinks />
-      <div>Attractinos page</div>
+      <div className="my-20">
+        <MsterialTable
+          title="Material Table"
+          data={data}
+          columns={columns}
+        ></MsterialTable>
+      </div>
     </div>
   );
 }
